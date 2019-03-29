779bdad0aabfe45760a27f0d81e9f077b0e17781
Larry Smith Jr <mrlesmithjr@gmail.com>
2019-03-29 19:37:38 -0400

    Trying to fix FreeNAS build

--- packer_builder/template.py
+++ packer_builder/template.py
@@ -174,10 +174,10 @@ class Template():
                         '<enter>',
                         '{{ user `password` }}<tab>{{ user `password` }}<tab><enter>',
                         '<enter>',
-                        '<wait60><wait60>',
+                        '<wait60><wait60><wait60>',
                         '<enter>',
                         '3<enter>',
-                        '<wait60><wait60><wait30>',
+                        '<wait60><wait60><wait60><wait60><wait60>',
                         '9<enter>',
                         'curl -X PUT -u {{ user `username` }}:{{ user `password` }} -H \'Content-Type: application/json\' -d \'{\"ssh_rootlogin\": true}\' http://localhost/api/v1.0/services/ssh/<enter>',
                         'curl -X PUT -u {{ user `username` }}:{{ user `password` }} -H \'Content-Type: application/json\' -d \'{\"srv_enable\": true}\' http://localhost/api/v1.0/services/services/ssh/<enter>'

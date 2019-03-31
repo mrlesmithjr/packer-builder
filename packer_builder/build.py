9bf86eb7245c788dc8c57eaa52e6da23770097bd
Larry Smith Jr <mrlesmithjr@gmail.com>
2019-03-30 23:43:36 -0400

    Added custom build manifest

--- packer_builder/build.py
+++ packer_builder/build.py
@@ -1,7 +1,10 @@
 """Build generated Packer template."""
 import os
+from datetime import datetime
+import time
 import subprocess
 import sys
+import json
 from .template import Template
 
 
@@ -11,15 +14,28 @@ class Build():
     def __init__(self, args, distros):
         self.distros = distros
         self.build_dir = args.outputdir
+        self.build_manifest_file = os.path.join(
+            self.build_dir, 'packer-builder.json')
         if args.distro is not None:
             self.distro = args.distro
         else:
             self.distro = 'all'
+        self.load_build_manifest()
         self.iterate()
+
+    def load_build_manifest(self):
+        if os.path.isfile(self.build_manifest_file):
+            with open(self.build_manifest_file, 'r') as stream:
+                self.build_manifest = json.load(stream)
+        else:
+            self.build_manifest = dict()
 
     def iterate(self):
         """Iterate through defined distros and build them."""
         for distro, distro_spec in self.distros.items():
+            distro_check = self.build_manifest.get(distro)
+            if distro_check is None:
+                self.build_manifest[distro] = {'builds': []}
             if self.distro == 'all' or (self.distro != 'all' and
                                         self.distro == distro):
                 for version, version_spec in distro_spec['versions'].items():
@@ -28,7 +44,16 @@ class Build():
                     self.builders = distro_spec['builders']
                     self.current_dir = os.getcwd()
                     self.validate()
+                    build_time_epoch = time.mktime(
+                        datetime.now().timetuple())
+                    build_info = {'version': str(version),
+                                  'build_time': str(build_time_epoch)}
+                    self.build_manifest[distro]['builds'].append(
+                        build_info)
                     self.build()
+                    with open(self.build_manifest_file, 'w') as stream:
+                        stream.write(json.dumps(
+                            self.build_manifest, indent=4))
 
     def validate(self):
         """Validate generated Packer template."""

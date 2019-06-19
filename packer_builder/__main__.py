651dd6c9aaa1800d3e72c3fd649060dfdf8a3395
Larry Smith Jr <mrlesmithjr@gmail.com>
2019-06-18 23:31:26 -0400

    Added functionality to generate templates only
    This will allow us to generate the template based on distros defined,
    validate the template, and then save as distro-version.json for each
    template.

--- packer_builder/__main__.py
+++ packer_builder/__main__.py
@@ -2,6 +2,7 @@
 from .args import get_args
 from .build import Build
 from .distros import Distros
+from packer_builder.generate_templates import GenerateTemplates
 
 
 def main():
@@ -12,6 +13,8 @@ def main():
         Build(args, distros)
     elif args.action == 'list-distros':
         Distros(args).list_distros()
+    elif args.action == 'generate-templates':
+        GenerateTemplates(args, distros)
 
 
 if __name__ == '__main__':
